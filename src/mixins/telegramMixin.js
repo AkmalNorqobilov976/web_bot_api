aad43545f2bdf72720466906f95549ca5482405e
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2023-01-07 09:52:56 +0500

    witout data

--- src/mixins/telegramMixin.js
+++ src/mixins/telegramMixin.js
@@ -1,45 +1,41 @@
+const tg = window.Telegram.WebApp
 export default {
-    data: () => {
-        return {
-            tg: window.Telegram.WebApp
-        }
-    },
     created() {
-        // if (Object.keys(this.tg.initDataUnsafe).length === 0) {
+        // if (Object.keys(tg.initDataUnsafe).length === 0) {
         //     // throw new Error("Ketaaa")
         // }
-        this.tg.ready();
-        this.tg.expand();
+        tg.ready();
+        tg.expand();
     },
     methods: {
         setParamsToMenuBtn: (params) => {
-            this.tg.MainButton.setParams({
+            tg.MainButton.setParams({
                 ...params
             })
         },
 
         onToggleMenuBtn: (params) => {
-            this.setParamsToMenuBtn(params);
-            if (this.tg.MainButton.isVisible) {
-                this.tg.MainButton.hide();
+            setParamsToMenuBtn(params);
+            if (tg.MainButton.isVisible) {
+                tg.MainButton.hide();
             } else {
-                this.tg.MainButton.show();
+                tg.MainButton.show();
             }
         },
         onHideMenuBtn: () => {
-            this.tg.MainButton.hide();
+            tg.MainButton.hide();
         },
         onShowMenuBtn: () => {
-            this.tg.MainButton.show();
+            tg.MainButton.show();
         },
         onSendDataTg(eventType, data) {
             if(eventType && data) {
-                this.tg.onEvent(eventType, this.sendDataTg(data));
-                this.tg.offEvent(eventType, this.sendDataTg(data));
+                tg.onEvent(eventType, sendDataTg(data));
+                tg.offEvent(eventType, sendDataTg(data));
             }
         },
         sendDataTg(data) {
-            this.tg.sendData(data);
+            tg.sendData(data);
         }
     }
 }
