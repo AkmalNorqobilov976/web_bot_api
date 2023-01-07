878c185c88c902e3bd22f8892177485fd47bc011
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2023-01-07 20:40:14 +0500

    hit

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,9 +3,9 @@ import axios from "axios";
 
 const { user } = useTelegram();
 user
-const BOT_TOKEN = "5861450281:AAEpZ3soHXG8xNrOK4ccDJezAPw8fEziMfk"
+const BOT_TOKEN = ""
 const service = axios.create({
-    baseURL: `https://api.telegram.org/${BOT_TOKEN}/sendMessage?chat_id=5882209120`
+    baseURL: `https://api.telegram.org/${BOT_TOKEN}/sendMessage`
 });
 
 
