453de7bdd6eb9ad507905d3639a3b0e203b60a2c
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2023-01-21 14:33:52 +0500

    commitable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,7 +27,7 @@
                         Market
                     </p>
                 </div>
-                <div class="home-button-tab__button" @click="$router.push('/statistics')">
+                <div class="home-button-tab__button" @click="$router.push('/statistics/preview/')">
                     <i class="ri-pie-chart-2-line"></i>
                     <p>
                         Statistika
