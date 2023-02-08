5d29b271a85270fed0aef6c1c22f6cca6d3903b3
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2023-02-08 16:51:42 +0500

    git aeldsjf

--- src/composables/useCopyToClipboard.js
+++ src/composables/useCopyToClipboard.js
@@ -3,11 +3,17 @@ import { useToastStore } from "@/store/useToastStore"
 export const useCopyToClipboard = () => {
     const toastStore = useToastStore();
     const copyToClipboard = (e, text) => {
-            
         navigator.clipboard.writeText(text)
             .then(() => {
                 toastStore.showToastAsAlert({
                     message: "Nusxalandi",
+                    delayTime: 3000,
+                    type: 'copy'
+                })
+            })
+            .catch((error) => {
+                toastStore.showToastAsAlert({
+                    message: error.message,
                     delayTime: 3000,
                     type: 'copy'
                 })
