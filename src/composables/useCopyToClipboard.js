b0f9753a9393319da5a4fa9454a3d96d451fe8ca
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2023-02-08 17:03:54 +0500

    gitcha

--- src/composables/useCopyToClipboard.js
+++ src/composables/useCopyToClipboard.js
@@ -3,21 +3,24 @@ import { useToastStore } from "@/store/useToastStore"
 export const useCopyToClipboard = () => {
     const toastStore = useToastStore();
     const copyToClipboard = (e, text) => {
-        navigator.clipboard.writeText(text)
-            .then(() => {
-                toastStore.showToastAsAlert({
-                    message: "Nusxalandi",
-                    delayTime: 3000,
-                    type: 'copy'
+        navigator.permissions.query({name: 'clipboard-write'})
+        .then(() => {
+            navigator.clipboard.writeText(text)
+                .then(() => {
+                    toastStore.showToastAsAlert({
+                        message: "Nusxalandi",
+                        delayTime: 3000,
+                        type: 'copy'
+                    })
                 })
-            })
-            .catch((error) => {
-                toastStore.showToastAsAlert({
-                    message: error.message,
-                    delayTime: 3000,
-                    type: 'copy'
+                .catch((error) => {
+                    toastStore.showToastAsAlert({
+                        message: error.message,
+                        delayTime: 3000,
+                        type: 'copy'
+                    })
                 })
-            })
+        })
     }
 
     return {
