a88be3fcd5caa9316defd3274b5fd64a7539fbea
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2023-02-13 21:39:04 +0500

    sldfj

--- src/composables/useTelegram.js
+++ src/composables/useTelegram.js
@@ -20,7 +20,7 @@ export function useTelegram() {
 
     const tgSetParamsToMainButton = ({text, textColor, color,  disabled}) => {
         tg.MainButton.setParams({
-            text: text,
+            text: `${color}`,
             text_color: textColor,
             color: color,
             is_active: !disabled,
