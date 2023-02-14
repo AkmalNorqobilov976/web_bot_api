b0bc6b903257df520a7945e4d291309949c9e17a
AkmalNorqobilov976 <norqobilovakmal976@gmail.com>
2023-02-14 19:53:47 +0500

    login

--- src/components/Form/inputs/VerifyInput.vue
+++ src/components/Form/inputs/VerifyInput.vue
@@ -123,4 +123,16 @@ export default defineComponent({
             }
         }
     }
+
+   input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
+        appearance: textfield;
+    }
 </style>
